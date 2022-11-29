302d0d575664529d511c49cdc7eca6cb6c3b1a42
Ole-Paul Heinzelmann <oheinzel@1-B-29.42heilbronn.de>
2022-11-29 18:11:50 +0100

    2022-11-29 18:11:50

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: oheinzel <oheinzel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/24 13:35:23 by oheinzel          #+#    #+#              #
-#    Updated: 2022/11/29 17:06:18 by oheinzel         ###   ########.fr        #
+#    Updated: 2022/11/29 18:11:13 by oheinzel         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,7 +26,7 @@ WHITE		= \033[0m
 
 #Sources
 
-SRC_FILES	=	operations main input
+SRC_FILES	=	operations main input mult_oprs
 
 
 SRC 		= 	$(addprefix $(SRC_DIR), $(addsuffix .c, $(SRC_FILES)))
--- src/mult_oprs.c
+++ src/mult_oprs.c
@@ -6,7 +6,7 @@
 /*   By: oheinzel <oheinzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 18:01:24 by oheinzel          #+#    #+#             */
-/*   Updated: 2022/11/29 18:05:09 by oheinzel         ###   ########.fr       */
+/*   Updated: 2022/11/29 18:11:37 by oheinzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ void	multi_swap(t_list **a, t_list **b)
 {
 	swap(a, 'x');
 	swap(b, 'x');
-	printf("ss\n");
+	ft_printf("ss\n");
 }
 
 void	multi_rotate(t_list **a, t_list **b)
 {
 	rotate(a, 'x');
 	rotate(b, 'x');
-	printf("rr\n");
+	ft_printf("rr\n");
 }
 
 void	multi_rrotate(t_list **a, t_list **b)
 {
 	rrotate(a, 'x');
 	rrotate(b, 'x');
-	printf("rrr\n");
+	ft_printf("rrr\n");
 }
