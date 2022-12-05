e1d0a9dfd4eccd04a20848d53001c2ad81f3c559
Ole-Paul Heinzelmann <oheinzel@1-B-29.42heilbronn.de>
2022-12-05 17:19:24 +0100

    2022-12-05 17:19:24

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: oheinzel <oheinzel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 11:40:46 by oheinzel          #+#    #+#             */
-/*   Updated: 2022/12/05 09:32:08 by oheinzel         ###   ########.fr       */
+/*   Updated: 2022/12/05 17:19:12 by oheinzel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,7 @@ int	check_dups(t_list *stack, int *argc)
 				return (1);
 			tmp = tmp->next;
 		}
-		*argc++;
+		(*argc)++;
 		stack = stack->next;
 	}
 	return (0);
